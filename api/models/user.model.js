a93f3e690ceed7bee1ec9420a28071c9107326e3
mertaltunma <mertaltunma@outlook.com>
2024-08-28 14:49:46 +0300

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true} // in order to be able to sort users by time and latest
+);
+
+const User=mongoose.model('User',userSchema);
+export default User;
