49963156b31c40afcb258208340c5db725d742f6
mertaltunma <mertaltunma@outlook.com>
2024-09-03 15:18:32 +0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,12 @@ mongoose
 
 const app = express();
 const port=3000;
+
+app.get('/hello', (req, res)=>{
+    res.send("Hello World");
+});
+
+app.use("/api/user",userRouter);
 
 app.listen(port,()=>{
     console.log(`Listenning on port ${port}`);
