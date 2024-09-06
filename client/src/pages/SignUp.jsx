9d9153996182aa8cf4272ea1611a1984205945ae
mertaltunma <mertaltunma@outlook.com>
2024-09-06 19:13:39 +0300

    complete sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,59 @@
-import {Link} from "react-router-dom"
+import {Link,useNavigate} from "react-router-dom"
+import {useState} from "react"
 
 export default function SignUp() {
+  const [formData,setFormData]=useState({});
+  const [error,setError]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
+
+  const handleChange=(e)=>{
+    setFormData(
+      {
+        ...formData, 
+        [e.target.id]: e.target.value,
+      }
+    )
+  };
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault(); 
+    try {
+      setLoading(true);
+      const res=await fetch("/api/auth/signup",
+        {
+          method: 'POST',
+          headers:{
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }
+      ); 
+      const data=await res.json();
+      if(data.sucess===false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in'); 
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className='p-3 max-w-xl mx-auto'>
       <h1 className="text-3xl text-center font-semibold my-7">Hesap Aç</h1>
-      <form className='flex flex-col gap-3'>
-        <input type="text" placeholder="Kullanıcı Adı" className='border p-3 rounded-xl' id="username"/>
-        <input type="text" placeholder="E-posta adresi" className='border p-3 rounded-xl' id="email"/>
-        <input type="text" placeholder="Şifre" className='border p-3 rounded-xl' id="password"/>
-        <button className='bg-slate-700 text-white p-3 rounded-xl uppercase hover:opacity-90'>Hesap Aç</button>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+        <input type="text" placeholder="Kullanıcı Adı" className='border p-3 rounded-xl' id="username" onChange={handleChange}/>
+        <input type="text" placeholder="E-posta adresi" className='border p-3 rounded-xl' id="email" onChange={handleChange}/>
+        <input type="text" placeholder="Şifre" className='border p-3 rounded-xl' id="password" onChange={handleChange}/>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-xl uppercase hover:opacity-90 disabled:opacity-80'>
+          {loading ? 'Yükleniyor...':'Hesap Aç'}
+        </button>
       </form>
       <div className="flex gap-3 mt-5">
         <p>Zaten hesabın var mı?</p>
@@ -16,6 +61,7 @@ export default function SignUp() {
           <span className="text-blue-800 hover:opacity-70">Giriş Yap</span>
         </Link>
       </div>
+      {error && <p className="text-red-600 mt-5">Farklı kullanıcı adı veya farklı e-posta adresi yazamayı deneyin.</p>}
     </div>
   )
 }
